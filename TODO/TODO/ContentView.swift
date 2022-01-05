6a7c4688e2266c2bbc5b3cfa5d67323892844cdc
studyLiyuhong165 <study_liyuhong165@163.com>
2022-01-05 22:54:50 +0800

    1.初始化项目

---
+++ TODO/TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO
+//
+//  Created by 李宇鸿 on 2022/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TODO/TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by 李宇鸿 on 2022/1/5.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
