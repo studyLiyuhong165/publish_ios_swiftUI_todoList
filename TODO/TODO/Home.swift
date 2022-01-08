f4942fe276bf6d6e0d1e64ce502ba87347e348cf
workliyuhong165 <work_liyuhong165@163.com>
2022-01-08 23:46:44 +0800

    home config
    home config

---
+++ TODO/TODO/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  TODO
+//
+//  Created by liyuhong2019 on 2022/1/8.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
